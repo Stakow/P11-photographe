25700cca476f35d7dc84a78f4011853e7fc7fecb
Lucas Szustakowski <szustakowskilucas@gmail.com>
2024-08-12 06:54:12 +0200

    header+footer

--- functions.php
+++ functions.php
@@ -1,2 +1,17 @@
 <?php
+// Ajouter le support pour les balises de titre
 add_theme_support('title-tag');
+
+// Enregistrer le menu principal
+function register_my_menu() {
+    register_nav_menus( array(
+        'main-menu' => __( 'Menu principal', 'text-domain' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'register_my_menu' );
+
+// Enregistrer et enfiler le fichier style.css
+function enqueue_my_styles() {
+    wp_enqueue_style( 'main-style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_my_styles' );
--- header.php
+++ header.php
@@ -1,10 +1,22 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head() ?>
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
 </head>
 <body>
+    <header>
+        <nav>
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'main-menu',
+                'menu_class'     => 'main-menu', // Classe CSS pour le menu
+            ) );
+            ?>
+        </nav>
+    </header>
+
     
